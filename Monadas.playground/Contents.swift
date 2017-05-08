412d78f24c90f72b8c0e1469264b5bce9c0de4b7
Swift Funcional <info@swiftfuncional.com>
2017-05-08 20:34:40 +0200

    Monads' asociative law

--- Monadas.playground/Contents.swift
+++ Monadas.playground/Contents.swift
@@ -4,3 +4,9 @@ import Foundation
 let content = NSURL(string: "http://swiftfuncional.com")
 	.flatMap { NSData(contentsOf: $0 as URL) }
 	.flatMap { NSString(data: $0 as Data, encoding: String.Encoding.utf8.rawValue) }
+
+let m1 = Int("3")
+let f1: (Int) -> Float? = { Float($0) }
+let f2: (Float) -> String? = { String($0) }
+
+m1.flatMap(f1).flatMap(f2) == m1.flatMap { x in f1(x).flatMap(f2) }

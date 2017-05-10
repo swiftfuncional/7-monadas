088cb6128d08f0f31278edeec92a82904bf11a36
Swift Funcional <info@swiftfuncional.com>
2017-05-10 12:31:19 +0200

    Change map and use flatMap

--- Monadas.playground/Sources/Result.swift
+++ Monadas.playground/Sources/Result.swift
@@ -3,12 +3,7 @@ public enum Result<S, E> {
 	case Failure(_: E)
 
 	public func map<T>(_ transform: (S) -> T) -> Result<T, E> {
-		switch self {
-		case let .Failure(reason):
-			return .Failure(reason)
-		case let .Success(value):
-			return .Success(transform(value))
-		}
+		return self.flatMap { .Success(transform($0))  }
 	}
 
 	public func flatMap<T>(_ transform: (S) -> Result<T, E>) -> Result<T, E> {
